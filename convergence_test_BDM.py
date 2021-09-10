b4ef54f5b38fdd08e810b7c42bc2241ba7f7a473
Kuijun Liu <kuijun.liu20@imperial.ac.uk>
2021-09-10 15:44:02 +0100

    BDM convergence test

--- convergence_test_BDM.py
+++ convergence_test_BDM.py
@@ -5,12 +5,13 @@ from split_initializations import *
 import math
 
 H = 10
-Dt = 0.00025
+Dt = 0.00005
 dt = Constant(Dt)
 errlist = []
 uerrors = []
 herrors = []
-N = range(10, 90, 10)
+N = [16, 32, 64, 128]
+# N = [16, 32, 64, 128]
 for n in N:
 
     Lx = 5000  # in km                           # Zonal length
@@ -116,10 +117,10 @@ for n in N:
     ens_0 = assemble(q**2 * D * dx)  # define enstrophy at each time step
     all_ens = []
 
-    name = "lsw_rk_BDM"
-    ufile = File("output/" + name + ".pvd")
+    # name = "lsw_rk_BDM_convergence_test"
+    # ufile = File("output/" + name + ".pvd")
 
-    tmax = 0.5  # days
+    tmax = 0.1  # days
     t = 0.
     dt1 = 1
     dumpfreq = 10
@@ -130,7 +131,7 @@ for n in N:
         global dumpcount
         dumpcount += 1
         if (dumpcount > dumpfreq):
-            ufile.write(u, D, q_out, time=t)
+            # ufile.write(u, D, q_out, time=t)
             dumpcount -= dumpfreq
 
 
@@ -153,28 +154,34 @@ for n in N:
         hlist.append(D)
     u0norm = norm(un0, norm_type="L2")
     h0norm = norm(Dn0, norm_type="L2")
+    # uerr = errornorm(ulist[-1], un0, norm_type="L2")
     uerr = errornorm(ulist[-1], un0, norm_type="L2")
     herr = errornorm(hlist[-1], Dn0, norm_type="L2")
     # norm1 = norm(ulist[-1]) + norm(hlist[-1])
     # uerrors.append(math.log(uerr))
-    uerrors.append(math.log(uerr/u0norm))
-    herrors.append(math.log(herr/h0norm))
+    uerrors.append((uerr/u0norm))
+    herrors.append((herr/h0norm))
 
 plt.figure()
 # plt.xticks([0, 1, 2, 3, 4, 5, 6, 7, 8], [60, '', '', 240, '', '', '', 480])
-plt.xticks([0, 1, 2, 3, 4, 5, 6, 7], [60, '', '', 240, '', '', '', 480])
-plt.plot(uerrors)
+# plt.xticks([0, 1], [180, 240])
+plt.xscale('log')
+plt.yscale('log')
+plt.plot(N, uerrors)
 # plt.xlabel('$\sqrt{n_{DOF}}$')
-plt.xlabel('square root of number of DOF')
+plt.xlabel('number of elements in one direction')
 plt.ylabel('L2 norm of relative velocity errors')
 # plt.grid(True)
 plt.show()
        
 plt.figure()
 # plt.xticks([0, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000], [0, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4])
-plt.xticks([0, 1, 2, 3, 4, 5, 6, 7], [60, '', '', 240, '', '', '', 480])
-plt.plot(herrors)
-plt.xlabel('square root of number of DOF')
+# plt.xticks([0, 1, 2, 3, 4, 5, 6, 7], [60, '', '', 240, '', '', '', 480])
+# plt.xticks([0, 1], [180, 240])
+plt.xscale('log')
+plt.yscale('log')
+plt.plot(N, herrors)
+plt.xlabel('number of elements in one direction')
 plt.ylabel('L2 norm of relative depth errors')
 # plt.grid(True)
 plt.show()
